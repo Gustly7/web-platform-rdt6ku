e2dac89630211f7d7a22f27eb7bcbccd83244dfe
Gustly7 <Gustly@yandex.ru>
2022-12-09 16:58:07 +0400

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello!');
+const div = document.getElementById('smiles');
+const main = document.getElementById('main');
+for (let i = 1536; i < 1592; i++) {
+  // div.append(i + '=' + String.fromCodePoint(0x1f000 + i) + '\n');
+}
+
+setInterval(() => {
+  main.innerHTML = String.fromCodePoint(
+    0x1f000 + 1536 + Math.floor(Math.random() * 56)
+  );
+}, 20);
